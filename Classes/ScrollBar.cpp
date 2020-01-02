196def78b557d0cd410b0629e0ca037bca2ce6c0
grimquarry <grimquarry@gmail.com>
2020-01-02 15:32:49 -0800

    Remove comments related to troubleshooting and add version info.

--- Classes/ScrollBar.cpp
+++ Classes/ScrollBar.cpp
@@ -1,15 +1,26 @@
+/**
+  Garden Space Planner
+  ScrollBar.cpp
+  Purpose: Source file for the ScrollBar class.  This class can be used to add
+  a scrollbar to content displayed in a sf::View object.
+
+  @author Jeremiah Kellogg
+  @version 1.0.0 01/02/20
+*/
 #include "ScrollBar.h"
 
+//Default Constructor
 ScrollBar::ScrollBar() { }
 
+//Constructor
 ScrollBar::ScrollBar(sf::View &view)
 {
-  //Scrolling and first click on scrollbar should not be true on instantiation of the object.
   m_isScrolling = false;
   m_firstClick = false;
   m_scrollBarView = view;
 }
 
+//Destructor
 ScrollBar::~ScrollBar() { }
 
 //Sets the m_firstClick member variable to true or false.
@@ -18,16 +29,10 @@ void ScrollBar::SetFirstClick(bool click)
   m_firstClick = click;
 }
 
-//Returns the value of the m_firstClick member variable.
-bool ScrollBar::GetFirstClick()
-{
-  return m_firstClick;
-}
-
 //Implements the logic behind scroll functionality
-void ScrollBar::Scroll(sf::RenderWindow &window, sf::RectangleShape &viewborder)
+void ScrollBar::Scroll(sf::RenderWindow &window)
 {
-  std::cout << "Scroll was called" << std::endl;
+  //std::cout << "Scroll was called" << std::endl;
   sf::Vector2i mouseWindowPostion = sf::Mouse::getPosition(window); //Grabs position of mouse in the window.  *****Should this be it's own function?  It's used in other spots, too*****
   sf::Vector2f mouseViewPosition = window.mapPixelToCoords(mouseWindowPostion);  //Translates mouse position in window to mouse position in the view. *****Should this be it's own function?  It's used in other spots, too*****
 
@@ -45,47 +50,46 @@ void ScrollBar::Scroll(sf::RenderWindow &window, sf::RectangleShape &viewborder)
   //What to do if scroll bar slider is within bounds of scroll bar container.
   if(m_mouseYNew - m_offset > m_scrollContainer.getPosition().y && m_mouseYNew - m_offset < m_scrollContainer.getSize().y - m_scrollElement.getSize().y)
   {
-    std::cout << "First if statment ran." << std::endl;
     m_scrollElement.setPosition(m_scrollElement.getPosition().x, m_mouseYNew - m_offset);
     m_centerScreen.setPosition(m_scrollBarView.getSize().x / 2.f, m_scrollBarView.getSize().y / 2 + m_scrollElement.getPosition().y * viewScrollSpeed);
   }
   //What to do if scroll bar slider is outside the max bounds of scrollbar container.
   else if(m_mouseYNew - m_offset > m_scrollContainer.getSize().y)
   {
-    std::cout << "First else if statement ran." << std::endl;
     m_scrollElement.setPosition(m_scrollMaximum);
     m_centerScreen.setPosition(m_scrollBarView.getSize().x / 2.f, (m_scrollBarView.getSize().y / 2.f) + m_scrollMaximum.y);
   }
   //What to do if scrollbar slider is below the minimum bounds of scrollbar container.
   else if (m_mouseYNew - m_offset < m_scrollContainer.getPosition().y)
   {
-    std::cout << "Second else if statment ran" << std::endl;
     m_scrollElement.setPosition(m_scrollMinimum);
     m_centerScreen.setPosition(m_scrollBarView.getSize().x / 2.f, (m_scrollBarView.getSize().y / 2.f));
   }
+
   m_firstClick = false;//first click is no longer true after the program loop runs once after scrollbar is clicked.
 }
 
 //Returns true or false depending on whether the mouse is positioned over the scrollbar or not.
-bool ScrollBar::MouseOverScroll(sf::RenderWindow &window)
+bool ScrollBar::MouseOverScroll(sf::RenderWindow &window, float offset)
 {
-  std::cout << "MouseOverScroll was called" << std::endl;
   sf::Vector2i mouseWindowPostion = sf::Mouse::getPosition(window); //Grabs position of mouse in the window.  *****Should this be it's own function?  It's used in other spots, too*****
   sf::Vector2f mouseViewPosition = window.mapPixelToCoords(mouseWindowPostion); //Translates mouse position in window to mouse position in the view.  *****Should this be it's own function?  It's used in other spots, too*****
 
-  std::cout << "mouseViewPostion X is: " << mouseViewPosition.x << std::endl;
-  std::cout << "mouseViewPostion Y is: " << mouseViewPosition.y << std::endl;
+  float adjustXPosition = window.getSize().x * offset;
+  mouseViewPosition.x -= adjustXPosition;
+
+  //std::cout << "mouseViewPostion X is: " << mouseViewPosition.x << std::endl;
+  //std::cout << "mouseViewPostion Y is: " << mouseViewPosition.y << std::endl;
   float scrollPosX = m_scrollElement.getPosition().x;
   float scrollXPosWidth = scrollPosX + m_scrollElement.getGlobalBounds().width;
-  std::cout << "Scroll element X is: " << scrollPosX << std::endl;
-  std::cout << "Scroll element Y is: " << m_scrollElement.getPosition().y << std::endl;
+  //std::cout << "Scroll element X is: " << scrollPosX << std::endl;
+  //std::cout << "Scroll element Y is: " << m_scrollElement.getPosition().y << std::endl;
 
   if(mouseViewPosition.x < scrollXPosWidth && mouseViewPosition.x > scrollPosX && mouseViewPosition.y < m_scrollElement.getPosition().y + m_scrollElement.getSize().y && mouseViewPosition.y > m_scrollContainer.getPosition().y)
   {
-    std::cout << "returned true" << std::endl;
     return true;
   }
-  std::cout << "returned false" << std::endl;
+
   return false;
 }
 
@@ -98,88 +102,98 @@ sf::Vector2f ScrollBar::GetScrollPosition(ContentContainer &container)
 //Sets whether or not the user is utilizing scroll functionality by passing the right value to the m_isScrolling member variable.
 void ScrollBar::SetScrolling(bool toScroll)
 {
-  std::cout << "SetScrolling was called and assigned: " << toScroll << std::endl;
   m_isScrolling = toScroll;
 }
 
 //Returns the value of the m_isScrolling member variable to determine if user is scrolling or not.
 bool ScrollBar::GetScrolling()
 {
-  //std::cout << "GetScrolling was called and returned value of: " << m_isScrolling << std::endl;
   return m_isScrolling;
 }
 
 //Sets the color of the scrollbar (used for changing scrollbar color when user clicks on it).
 void ScrollBar::SetElementColor(sf::Color color)
 {
-  std::cout << "SetElementColor was called" << std::endl;
   m_scrollElement.setFillColor(color);
 }
 
+//Sets the color of the scrollbar Container).
 void ScrollBar::SetContainerColor(sf::Color color)
 {
   m_scrollContainer.setFillColor(color);
 }
 
+//Sets the size of a scrollbar container.
 void ScrollBar::SetContainerSize (sf::Vector2f size)
 {
   m_scrollContainer.setSize(size);
 }
 
+//Returns the size of the m_scrollContainer member (scroll bar container size).
 sf::Vector2f ScrollBar::GetContainerSize()
 {
   return m_scrollContainer.getSize();
 }
 
+//Sets the position of the Scrollbar container.
 void ScrollBar::SetContainerPos(sf::Vector2f position)
 {
   m_scrollContainer.setPosition(position);
 }
 
+//Returns the position of the scrollbar container (m_scrollContainer).
 sf::Vector2f ScrollBar::GetContainerPos()
 {
   m_scrollContainer.getPosition();
 }
 
+//Sets the minimum position the scrollbar slider can move to.
 void ScrollBar::SetScrollMin()
 {
   m_scrollMinimum.x = m_scrollContainer.getPosition().x * 1.015f;
   m_scrollMinimum.y =  m_scrollContainer.getPosition().y;
 }
 
+//Returns the minimum position the scrollbar slider can move to.
 sf::Vector2f ScrollBar::GetScrollMin()
 {
   return m_scrollMinimum;
 }
 
+//Sets the maximum position the scrollbar slider can move to.
 void ScrollBar::SetScrollMax()
 {
   m_scrollMaximum.x = m_scrollContainer.getPosition().x * 1.01f;
   m_scrollMaximum.y = m_scrollContainer.getGlobalBounds().height;
 }
 
+//Sets the size of the scrollbar slider (m_scrollElement).
 void ScrollBar::SetElementSize(sf::Vector2f size)
 {
   m_scrollElement.setSize(size);
 }
 
+//Sets the position of the scrollbar slider (m_scrollElement).
 void ScrollBar::SetElementPos(sf::Vector2f position)
 {
   m_scrollElement.setPosition(position);
 }
 
+//Sets the position of where the view should be in relation to the scrollbar.
 void ScrollBar::SetCenterScreen(float x, float y)
 {
   m_centerScreen.setPosition(x, y);
 }
 
-void ScrollBar::SetView(sf::View &view)
-{
-  m_scrollBarView = view;
-}
-
+//Draws the scrollbar to the appropriate view.
 void ScrollBar::Draw(sf::RenderWindow &window)
 {
   window.draw(m_scrollContainer);
   window.draw(m_scrollElement);
+}
+
+//Sets the view where the scrollbar will be used.
+void ScrollBar::SetView(sf::View &view)
+{
+  m_scrollBarView = view;
 }
--- Classes/ScrollBar.h
+++ Classes/ScrollBar.h
@@ -1,7 +1,17 @@
+/**
+  Garden Space Planner
+  ScrollBar.h
+  Purpose: Header file for the ScrollBar class.  This class can be used to add
+  a scrollbar to content displayed in a sf::View object.
+
+  @author Jeremiah Kellogg
+  @version 1.0.0 01/02/20
+*/
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "ContentContainer.h"
+#include "Map.h"
 
 class ScrollBar
 {
@@ -27,24 +37,21 @@ public:
   void SetFirstClick(bool click);
 
   /**
-    Returns the value of the m_firstClick member variable.
-  */
-  bool GetFirstClick();
-
-  /**
     Implements the logic behind scroll functionality
 
     @param &window The sf::RenderWindow object the scrollbar should be drawn to.
-    @param &viewborder References the sf::RectangleShape object set as a border for the left column.
   */
-  void Scroll(sf::RenderWindow &window, sf::RectangleShape &viewborder);
+  void Scroll(sf::RenderWindow &window);
 
   /**
     Returns true or false depending on whether the mouse is positioned over the scrollbar or not.
 
     @param &window The sf::RenderWindow object the scrollbar is drawn to.
+    @param offset Optional parameter to fix weird offset between mouse position and scroll slider (scroll element) position
+      happening in the SelectMap instatiation of a scrollbar.  This should be investigated further.
+      ********************It might be tied to how mapdisplay containers are positioned?*********************
   */
-  bool MouseOverScroll(sf::RenderWindow &window);
+  bool MouseOverScroll(sf::RenderWindow &window, float offset = 0);
 
   /**
     Returns an sf::Vector2f object containing x and y axis position coordinates of the scrollbar.
@@ -66,43 +73,122 @@ public:
   bool GetScrolling();
 
   /**
-    Sets the color of the scrollbar (used for changing scrollbar color when user clicks on it).
+    Sets the color of the scrollbar slider (used for changing scrollbar color when user clicks on it).
 
     @param color The sf::Color object passed to m_scrollElement (scrollbar) to set the fill color.
   */
   void SetElementColor(sf::Color color);
 
+  /**
+    Function template that updates size and position of scroll area when a new sf::RectangleShape object is added to
+      or removed from the scrollable area.  It resizes things when new maps or plants are added.
 
+    @param &newItem References a clickable container used to display the new map or plant.
+    @param &displayArea References an object storing the size and location of scrollable content in a view.
+    @param &vector References a vector that can be used to update and calculate the new size of a displayArea.
+    @param &viewScreenRatio References a screen to view ratio used to calculate and update scroll info.
+  */
+  template<typename itemVector>
+  void UpdateScrollArea(sf::RectangleShape &newItem, sf::RectangleShape &displayArea, std::vector<itemVector> &vector, float &screenViewRatio)
+  {
+    newItem.setSize({m_scrollBarView.getSize().x - (m_scrollBarView.getSize().x * .02f), m_scrollBarView.getSize().y / 4});
+    displayArea.setSize({m_scrollBarView.getSize().x, newItem.getSize().y * vector.size()});
+    m_scrollContainer.setSize({m_scrollBarView.getSize().x * .02f, displayArea.getSize().y});
+    m_scrollElement.setSize({m_scrollContainer.getSize().x * .75f, m_scrollBarView.getSize().y / screenViewRatio});
+    m_scrollMinimum.x = m_scrollContainer.getPosition().x * 1.0032f;
+    m_scrollMinimum.y =  m_scrollContainer.getPosition().y;
+    m_scrollMaximum.x = m_scrollContainer.getPosition().x * 1.01f;
+    m_scrollMaximum.y = m_scrollContainer.getGlobalBounds().height;
+  };
+
+  /**
+    Sets the color of the scrollbar Container).
+
+    @param color The sf::Color object passed to m_scrollContainer (scrollbar) to set the fill color.
+  */
   void SetContainerColor(sf::Color color);
 
+  /**
+    Sets the size of a scrollbar container.
+
+    @param size The sf::Vector2f object passed to m_scrollContainer to set the size.
+  */
   void SetContainerSize(sf::Vector2f size);
 
+  /**
+    Returns the size of the m_scrollContainer member (scroll bar container size).
+  */
   sf::Vector2f GetContainerSize();
 
+  /**
+    Sets the position of the Scrollbar container.
+
+    @param position The sf::Vector2f value passed to m_scrollContainer to position it.
+  */
   void SetContainerPos(sf::Vector2f position);
 
+  /**
+    Returns the position of the scrollbar container (m_scrollContainer).
+  */
   sf::Vector2f GetContainerPos();
 
+  /**
+    Sets the minimum position the scrollbar slider can move to.
+  */
   void SetScrollMin();
 
+  /**
+    Returns the minimum position the scrollbar slider can move to.
+  */
   sf::Vector2f GetScrollMin();
 
+  /**
+    Sets the maximum position the scrollbar slider can move to.
+  */
   void SetScrollMax();
 
+  /************WHY IS THERE NO SCROLLMAX??? Might be part of the program's bugginess?**********/
+
+  /**
+    Sets the size of the scrollbar slider (m_scrollElement).
+
+    @param size The sf::Vector2f value that sets the size of the slider (m_scrollElement).
+  */
   void SetElementSize(sf::Vector2f size);
 
+  /**
+    Sets the position of the scrollbar slider (m_scrollElement).
+
+    @param position The sf::Vector2f value that sets the position of the slider (m_scrollElement).
+  */
   void SetElementPos(sf::Vector2f position);
 
+  /**
+    Sets the position of where the view should be in relation to the scrollbar.
+
+    @param x The horizontal center position where the view should focusing.
+    @param y The verticle center position where the view should focusing.
+  */
   void SetCenterScreen(float x, float y);
 
+  /**
+    Draws the scrollbar to the appropriate view.
+
+    @param &window References the window the scrollbar will be drawn.
+  */
   void Draw(sf::RenderWindow &window);
 
+  /**
+    Sets the view where the scrollbar will be used.
+
+    @param &view References the proper view.
+  */
   void SetView(sf::View &view);
 
 private:
   bool m_isScrolling;  //Member variable identifying if the user is scrolling or not.
   bool m_firstClick;  //Member variable identifying if a user is clicking on the scrollbar from a non scrolling state.
-  sf::View m_scrollBarView; //Object identifyinng the view the left column should be associated with.
+  sf::View m_scrollBarView; //Object identifyinng the view the scrollbar should be associated with.
   float m_mouseYNew; //Most current postion of the mouse's y coordinate position.
   float m_offset; //Offset between where the user clicked the scrollbar and the position of the scrollbar.
   sf::RectangleShape m_scrollElement; //Object for the scrollbar slider.
@@ -110,5 +196,4 @@ private:
   sf::RectangleShape m_centerScreen;//Object that determines where the view camera is positioned over the content display.
   sf::Vector2f m_scrollMaximum; //Object with x & y coordinates for maximum scrollbar position.
   sf::Vector2f m_scrollMinimum; //Object with x & coordinates for minimum scollbar position.
-
 };
